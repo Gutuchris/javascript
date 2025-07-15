d8bb076c2d5405238370841b811fa5b8abd6dc5c
Chris Gutu <gutuchris199@gmail.com>
2025-07-15 21:37:07 +0300

    fourth_commit

--- arrays.js
+++ arrays.js
@@ -147,3 +147,54 @@ total.reverse();
 console.log(total)
 
 
+
+/*
+
+QUESTION
+
+You are given 2 arrays:
+
+let cart1 = ["apple", "banana"];
+let cart2 = ["orange", "grape"]
+
+Required:
+1.Add kiwi to the end of the cart1.
+2.Add mango to the beginning of cart2.
+3.Remove the last item from cart1 and the first item from cart2.
+4.Combine cart1 and cart2 into a new array callled finalCart.
+5.Check if banana is still in finalCart.
+6.Slice the middle two fruits from finalCart into a new array middleFruits.
+7.Reverse finalCart.
+8.Convert all items in middleFruits to uppercase.
+9.Replace GRAPE with PINEAPPLE in the middLeFruits.
+10.Suppose each fruit in finalCart cocts $2.345 ,calculate the total cost and round it to 2 decimal places 
+using number methods
+
+NB - Use console.log to display each soln
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
